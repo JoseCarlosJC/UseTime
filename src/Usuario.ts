3bbea2e0047b1bc010bfea7801f764bc7be54bd1
Francisco Javier Romero García <fjromeero@correo.ugr.es>
2022-11-04 12:27:08 +0100

    Avanzo #9 con la primera aproximación de la implementación de la clase Usuario

---
+++ src/Usuario.ts
@@ -0,0 +1,25 @@
+import { Rutina } from "./Rutina";
+import { Trabajo } from "./Trabajo";
+
+
+// Representa un usuario
+export class Usuario{
+
+    // Correo electrónico del usuario: se usará como identificador de este ya que es único para cada uno
+    correo: string;
+
+    // Contraseña
+    password: string;
+
+    // Intervalos de tiempo que estarán siempre ocupados.
+    accionesRutinarias: Array<Rutina>;
+
+    // Trabajos que se desean distribuir en tareas
+    trabajos: Array<Trabajo>
+
+    constructor(correo: string, password: string){
+        this.correo = correo;
+        this.password = password;
+    }
+
+}
