0e7f2f0c131f00a433712e77d297f28241070db6
Francisco Javier Romero García <fjromeero@correo.ugr.es>
2022-11-11 09:23:20 +0100

    Avanzo #9 y #10 cambiando archivo que representa intervalos libres

---
+++ src/Intervalos.ts
@@ -0,0 +1,16 @@
+
+// Representa aquellos intervalos de tiempo que estarán ocupados todas las semanas
+export class Rutina{
+    
+    // Hora a la que comienza la actividad rutinaria
+    #horaComienzo: number;
+
+    // Hora a la que termina
+    #horaFinalizacion: number;
+
+    constructor(horaComienzo: number, horaFinalizacion: number){
+        this.#horaComienzo = horaComienzo;
+        this.#horaFinalizacion = horaFinalizacion;
+    }
+
+}
