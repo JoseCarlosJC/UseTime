b4e5344c1411c45c5db778ad06ebad9e8aae59a6
Francisco Javier Romero García <fjromeero@correo.ugr.es>
2022-11-22 23:37:00 +0100

    Close #15 Creada clase que resuelve el problemas Se crea una clase usuario ,la cual incluye un atributo correo, que se usará para distinguir a los usuario, lo cual será necesario a la hora de crear tareas compartidas, igual de necesario para esto será el array de huecos libres en los días y el vector de trabajos.

---
+++ src/usuario.ts
@@ -0,0 +1,19 @@
+import { HuecosLibresDia } from "./huecos_libres_dia";
+import { Trabajo } from "./trabajo";
+
+// Representa un usuario
+export class Usuario{
+
+    // Correo electrónico del usuario: se usará como identificador de este ya que es único para cada uno
+    private _correo: string;
+
+    // Intervalos de tiempo de un dia que estarán libres.
+    private _huecosLibresDia: Array<HuecosLibresDia>;
+
+    // Trabajos que se desean distribuir en tareas
+    private _trabajos: Array<Trabajo>;
+
+    constructor(correo: string, password: string){
+        this._correo = correo;
+    }
+}
