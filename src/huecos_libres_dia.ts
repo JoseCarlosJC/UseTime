d11f0a20030da6ec79d98f956c32d8294eaf4517
Francisco Javier Romero García <fjromeero@correo.ugr.es>
2022-11-24 13:47:56 +0100

    Cambio archivos de acorde a los cambios anteriores

--- src/huecos_libres_dia.ts
+++ src/huecos_libres_dia.ts
@@ -1,4 +1,4 @@
-import { Hora } from "./hora";
+import { TramoHorario } from "./tramo_horario";
 
 // Representa aquellos intervalos de tiempo de un dia en los que se puede asignar una tarea
 export class HuecosLibresDia{
@@ -7,9 +7,9 @@ export class HuecosLibresDia{
     private _dia: Date;
 
     // Array de intervalos de tiempo asignables a tareas
-    private _intervalos: Array<[Hora,Hora]>
+    private _intervalos: Array<TramoHorario>
 
-    constructor(dia: Date, intervalos: Array<[Hora,Hora]>){
+    constructor(dia: Date, intervalos: Array<TramoHorario>){
         this._dia = dia;
         this._intervalos = intervalos;
     }
